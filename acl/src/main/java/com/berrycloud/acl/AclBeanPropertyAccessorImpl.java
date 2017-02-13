f2a88df0b9fd7f2f0568b8d23a6e2f031e497697
Selindek <istvan@berrycloud.co.uk>
2017-02-13 22:04:14 +0100

    cleanup

--- acl/src/main/java/com/berrycloud/acl/AclBeanPropertyAccessorImpl.java
+++ acl/src/main/java/com/berrycloud/acl/AclBeanPropertyAccessorImpl.java
@@ -7,8 +7,6 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.From;
-import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
@@ -18,14 +16,14 @@ import org.springframework.hateoas.Identifiable;
 
 import com.berrycloud.acl.domain.AclEntity;
 
-public class AclBeanPropertyAccessorImpl implements AclBeanPropertyAccessor{
+public class AclBeanPropertyAccessorImpl implements AclBeanPropertyAccessor {
 
   @PersistenceContext
   private EntityManager em;
 
   @Autowired
   private AclUserPermissionSpecification aclUserPermissionSpecification;
-  
+
   @Override
   public void setProperty(PersistentProperty<?> property, Object bean, Object value) {
     // TODO Auto-generated method stub
@@ -39,25 +37,17 @@ public class AclBeanPropertyAccessorImpl implements AclBeanPropertyAccessor{
     String propertyName = property.getName();
 
     Identifiable<Serializable> owner = (Identifiable<Serializable>) bean;
-    
-    CriteriaQuery<Object> query =  (CriteriaQuery<Object>) cb.createQuery(property.getActualType());
-    
-//    Root<AclEntity<Serializable>> root =  (Root<AclEntity<Serializable>>)query.from(property.getActualType());
-    Root<AclEntity<Serializable>> ownerRoot = (Root<AclEntity<Serializable>>) query.from(property.getOwner().getType());
-//    Predicate aclPredicate = aclUserPermissionSpecification.toPredicate(root, query, cb);
-//    query.select(root).where(cb.and(aclPredicate,cb.equal(ownerRoot.get("id"), owner.getId()),root.in(ownerRoot.join(propertyName))));
 
-    From<Root<AclEntity<Serializable>>,Root<AclEntity<Serializable>>> join = ownerRoot.join(propertyName);
-    query.select(join);
-    ((Join)join).on(cb.equal(ownerRoot.get("id"), owner.getId()));
-    Predicate aclPredicate = aclUserPermissionSpecification.toPredicate(ownerRoot, query, cb);
-    query.where(cb.and(aclPredicate/*,cb.equal(ownerRoot.get("id"), owner.getId())*/));
-    
-    //query.distinct(true);
-    
+    CriteriaQuery<Object> query = cb.createQuery(/* property.getActualType() */);
+
+    Root<?> ownerRoot = query.from(property.getOwner().getType());
+
+    query.select(ownerRoot.join(propertyName)/*.on(cb.equal(ownerRoot.get("id"), owner.getId()))*/);// has a WARNIG, but maybe faster
+    Predicate aclPredicate = aclUserPermissionSpecification.toPredicate((Root<AclEntity<Serializable>>) ownerRoot, query, cb);
+    query.where(cb.and(aclPredicate ,cb.equal(ownerRoot.get("id"), owner.getId()) )); // <- No WARNING, but I think it's slower
+
     TypedQuery<?> tq = em.createQuery(query);
-    
-    
+
     if (property.isCollectionLike()) {
       return tq.getResultList();
     }
@@ -66,6 +56,5 @@ public class AclBeanPropertyAccessorImpl implements AclBeanPropertyAccessor{
     }
     return tq.getSingleResult();
   }
-
 
 }
